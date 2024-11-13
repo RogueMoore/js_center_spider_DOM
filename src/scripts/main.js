d31a030ccaf3164e4327b32338ad2f5d7be667b3
Vitaly Grodetsky <rogue.moore.private@gmail.com>
2024-11-13 12:15:48 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const spiderSize = 50;
+const wallSize = 400;
+const offset = (wallSize - spiderSize) / 2;
+
+spider.style.top = `${offset}px`;
+spider.style.left = `${offset}px`;
